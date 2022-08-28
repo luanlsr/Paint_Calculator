3d58e74094fe1dc60baeb4652c9b51eb1fbe0b2d
luanfswd <luanfswd@gmail.com>
2022-08-28 02:29:30 -0300

    Updates

---
+++ precache-manifest.cf54fdfb32a69c973a27f688d996a3a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c9b524a945d6b24633b",
+    "url": "/Paint_Calculator/static/css/main.30d24f7c.chunk.css"
+  },
+  {
+    "revision": "6c9b524a945d6b24633b",
+    "url": "/Paint_Calculator/static/js/main.d08532b3.chunk.js"
+  },
+  {
+    "revision": "9d3c74d2ddc03694def4",
+    "url": "/Paint_Calculator/static/js/runtime~main.71087b74.js"
+  },
+  {
+    "revision": "6c90eb28818ef3aa262b",
+    "url": "/Paint_Calculator/static/js/2.19e1b9f4.chunk.js"
+  },
+  {
+    "revision": "ecd72f45bd2fbd385b9c8ef4a0740aa2",
+    "url": "/Paint_Calculator/index.html"
+  }
+];
